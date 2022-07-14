090bbe5352401cb47d6b821534e8aaaf765c4735
rere <regis-graptin@sfr.fr>
2022-07-14 16:33:57 +0200

    Revert "add header image and basic ui search element"
    This reverts commit f37c446c0101f9ccc443a4c2e39af186c9db419c.

--- src/components/HeaderSearch.vue
+++ src/components/HeaderSearch.vue
@@ -3,77 +3,21 @@
   <div class="large-container">
     <div id="header-search-image">
       <div id="header-search-container">
-        <div class="container">
-
-          <!-- Navigation -->
-          <nav class="navbar navbar-expand-lg">
-            <a class="navbar-brand" href="#">Car Rental</a>
-            
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler"
-              aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarToggler">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                  <a class="nav-link" href="#">Find your car</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">How it works</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-
-
-          <h1>Rent you car for your next vacation</h1>
-          <p>Rent a car has not be so easely</p>
-        </div>
+          <div class="container">
+              <h1>Decentralize car sharing</h1>
+              <p>Rent a car has not be so easely</p>
+          </div>
       </div>
     </div>
   </div>
-
-  <!-- Search component -->
-  <div id="search-form" class="container">
-    
-    <input v-model="city" placeholder="Where" />
-
-    <input v-model="starting_date" placeholder="Departure" />
-    <input v-model="ending_date" placeholder="Arrival" />
-
-    <select v-model="typeOfVehicle">
-      <option value="">Vehicle</option>
-      <option>Doesn't matter</option>
-      <option>Car</option>
-      <option>Truck</option>
-    </select>
-
-    <button v-on:click="search_car">Search</button>
-
-  </div>
-
-
 </template>
 
 <script>
 export default {
-  data ()  {
-    return {
-      city: "",
-      starting_date: "",
-      ending_date: "",
-      typeOfVehicle: "",
-    }
-  },
-  methods: {
-    search_car: function () {
-      console.log(this.city, this.starting_date, this.ending_date, this.typeOfVehicle);
-    }
-  }
 
 }
 </script>
 
 <style>
+
 </style>
